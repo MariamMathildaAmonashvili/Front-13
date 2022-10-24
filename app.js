3bc8faca6555e572076cd00ed79aeb587710cb20
mathildamus <113375599+MariamMathildaAmonashvili@users.noreply.github.com>
2022-10-24 15:30:15 +0400

    final product

---
+++ app.js
@@ -0,0 +1,29 @@
+let myName = "Mariami";
+let myLastName = "Amonashvili";
+const yearOfBirth = 2000;
+let myHobby = " Making Music.";
+let currentYear = 2022; 
+let myCity = "Tbilisi";
+let birthDate = "December";
+let birthDay = "23rd"
+
+
+console.log  (  myName , myLastName );
+
+
+let myAge = currentYear - yearOfBirth
+
+console.log (myAge)
+
+const text1 = "Hello, my name is " + myName + ",";
+const text2 = "I was born in " + birthDate + " " + birthDay + " " + birthDate  + " ,"  ; 
+const text3 = " currently I live in " + myCity  + ",";
+const text4 = " my hobby is" + " "  + myHobby ;  
+
+
+console.log ( text1, text2 , text3 ,text4 ) ;
+
+
+const newText =  `${text1}  ${text2}  ${text3}  ${text4}` ;
+
+console.log (newText)
